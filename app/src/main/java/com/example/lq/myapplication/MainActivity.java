407c87e1afa481bd4b650c76c1fc7e919d18a4c0
langqiang <langqiang@lespark.us>
2018-03-16 11:07:02 +0800

    [还是测试rebase]

--- app/src/main/java/com/example/lq/myapplication/MainActivity.java
+++ app/src/main/java/com/example/lq/myapplication/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //b
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
